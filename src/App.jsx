cd739312d759f22b7b45ec0d140ac9711e10004b
Abu Raihan <aburaihancse066@gmail.com>
2023-10-10 22:57:07 +0600

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer";
+import Error from "./pages/Error";
 
 
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold ">App is Running</h1>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/create-post" element={<CreatePost/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/*" element={<Error/>}/>
+      </Routes>
+      <Footer/>
     </>
   )
 }
